a3f5134aefe9d0b809e2962f28e6144fe7621e93
Mohsen Karimi <karimimohsen@live.com>
2018-05-05 19:25:27 +0430

    complete

--- src/register.js
+++ src/register.js
@@ -3,6 +3,6 @@ import Child1 from './child1.js';
 import Child2 from './child2.js';
 
 
-window.customElements.define('parent-component',ParentComponent);
-window.customElements.define('x-child1',Child1);
-window.customElements.define('x-child2',Child2);
+window.customElements.define('parent-component', ParentComponent);
+window.customElements.define('x-child1', Child1);
+window.customElements.define('x-child2', Child2);
